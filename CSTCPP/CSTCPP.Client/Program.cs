d143cd1714cad718b1c83e3d64c726af206811d0
Feiron <53085628+fr1g@users.noreply.github.com>
2025-03-03 14:55:03 +0300

    init

---
+++ CSTCPP/CSTCPP.Client/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+starting:
+Console.WriteLine("CSTCPP CLIENT\nAutomatically using port 11451 as server's port.");
+IPAddress? ip = null;
+Socket? client = null;
+IPEndPoint? endpoint = null;
+while (ip == null || endpoint == null || client == null)
+{
+    Console.WriteLine("Enter IP of target server: ");
+    Console.Write("<<< ");
+    var target = Console.ReadLine();
+    try
+    {
+        ip = IPAddress.Parse(target);
+        
+        endpoint = new(ip, 11451);
+        client = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        client!.Connect(endpoint);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        ip = null;
+        Console.Error.WriteLine($"INVALID IP ADDRESS: {target} @ NOT FOUND or WRONG FORMAT \n");
+        continue;
+    }
+}
+
+Console.WriteLine("\nReady.\n");
+
+while (true)
+{
+    Console.Write("<<< ");
+    string read = Console.ReadLine() ?? "default_empty_message";
+    if (read == "") 
+        read = "default_empty_message";
+    try
+    {
+        client.Send(Encoding.UTF8.GetBytes(read));
+        var buff = new byte [1024 * 1024];
+        int parse = client.Receive(buff);
+        var res = Encoding.UTF8.GetString(buff, 0, parse);
+        Console.WriteLine($"from server >>> {res}\n");
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine($"\n===ERR CRIT===\nconnection lost.\n{ex}\n=== EC ===\n");
+        goto starting;
+    }
+
+
+    
+}
---
+++ CSTCPP/CSTCPP.Server/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("CSTCPP Server side...\nThis program will bind on port: 11451, timeout: 4");
+
+Socket listener = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPAddress addr = IPAddress.Any;
+IPEndPoint endPoint = new(addr, 11451);
+
+try
+{
+    listener.Bind(endPoint);
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine("Failed to bind on the port 11451");
+    Console.WriteLine(ex);
+    throw;
+}
+
+listener.Listen(10);
+
+
+List<EndPoint> connected = [];
+
+Console.WriteLine($"Now started listening on the xxx");
+
+Socket communication = listener.Accept();
+
+while (true)
+{
+    // communication = listener.Accept();
+    Console.WriteLine($"inbound client: {communication.RemoteEndPoint}");
+    if (connected.IndexOf(communication.RemoteEndPoint!) > 0)
+    {
+        Console.WriteLine("inbound but busy.");
+        communication.Send(Encoding.UTF8.GetBytes($"Server received msg:::BUSY"));
+        communication.Close();
+        continue;
+    }
+    else
+    {
+        if(connected.IndexOf(communication.RemoteEndPoint!) == -1) 
+            connected.Add(communication.RemoteEndPoint!);
+        
+    }
+    var buff = new byte [1024 * 1024];
+    int parse = communication.Receive(buff);
+    var content = Encoding.UTF8.GetString(buff, 0, parse);
+    if (content == "/end")
+    {
+        connected.Remove(communication.RemoteEndPoint!);
+        communication.Send(Encoding.UTF8.GetBytes($"Server msg:::CLOSING_CONNECTION"));
+        communication.Close();
+        continue;
+    }
+    Console.WriteLine($"Msg::client >>> {content}");
+    communication.Send(Encoding.UTF8.GetBytes($"Server received msg OK <<< {content}"));
+    // communication.Disconnect(true);
+    GC.Collect();
+}
+
